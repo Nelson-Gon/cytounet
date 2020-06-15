2b1b0eaa3052eb9cc00f56e444370374d84daa65
Nelson-Gon <gonzabato@hotmail.com>
2020-06-15 08:51:02 +0800

    try batch norm

--- data.py
+++ data.py
@@ -25,7 +25,7 @@ COLOR_DICT = np.array([Sky, Building, Pole, Road, Pavement,
 
 def adjustData(img,mask,flag_multi_class,num_class):
     if(flag_multi_class):
-        img = img / 255
+        img = img / 255.
         mask = mask[:,:,:,0] if(len(mask.shape) == 4) else mask[:,:,0]
         new_mask = np.zeros(mask.shape + (num_class,))
         for i in range(num_class):
@@ -37,8 +37,8 @@ def adjustData(img,mask,flag_multi_class,num_class):
         new_mask = np.reshape(new_mask,(new_mask.shape[0],new_mask.shape[1]*new_mask.shape[2],new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask,(new_mask.shape[0]*new_mask.shape[1],new_mask.shape[2]))
         mask = new_mask
     elif(np.max(img) > 1):
-        img = img / 255
-        mask = mask /255
+        img = img / 255.
+        mask = mask /255.
         mask[mask > 0.5] = 1
         mask[mask <= 0.5] = 0
     return (img,mask)
@@ -114,7 +114,7 @@ def labelVisualize(num_class,color_dict,img):
     img_out = np.zeros(img.shape + (3,))
     for i in range(num_class):
         img_out[img == i,:] = color_dict[i]
-    return img_out / 255
+    return img_out / 255.
 
 
 
