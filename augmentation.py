8029d3e2e25ef344cfb6ace7e485ba568330e32a
Nelson-Gon <gonzabato@hotmail.com>
2020-06-16 20:34:01 +0800

    Fix augmentation view

--- augmentation.py
+++ augmentation.py
@@ -3,15 +3,20 @@ import glob
 from skimage.io import ImageCollection
 import matplotlib.pyplot as plt
 
+
 # augment data
 
-def show_augmented(directory = "aug", image_suffix= "png", image_type="masks", number = 4):
+def show_augmented(directory="aug", image_suffix="png", image_type="masks", number=4):
     masks = ImageCollection(glob.glob(directory + "/mask*." + image_suffix))
-    original = ImageCollection(glob.glob(directory + "/image/*." + image_suffix ))
-    image_type = {'masks': masks, "original": original}
-    plt.figure(figsize = (10, 10))
+    original = ImageCollection(glob.glob(directory + "/image/*." + image_suffix))
+    use_data = original
+    if image_type == "masks":
+        use_data = masks
+
+    plt.figure(figsize=(10, 10))
     for i in range(number):
         plt.subplot(number / 2, number / 2, i + 1)
-        plt.imshow(image_type[i])
+        plt.imshow(use_data[i])
+
 
 plt.show()
