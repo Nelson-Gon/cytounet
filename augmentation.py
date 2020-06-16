5fb27bb5cdba05a956543b12e4464c8e28db2566
Nelson-Gon <gonzabato@hotmail.com>
2020-06-16 20:45:15 +0800

    Use explicit else

--- augmentation.py
+++ augmentation.py
@@ -9,9 +9,10 @@ import matplotlib.pyplot as plt
 def show_augmented(directory="aug", image_suffix="png", image_type="masks", number=4):
     masks = ImageCollection(glob.glob(directory + "/mask*." + image_suffix))
     original = ImageCollection(glob.glob(directory + "/image/*." + image_suffix))
-    use_data = original
     if image_type == "masks":
         use_data = masks
+    else:
+        use_data = original
 
     plt.figure(figsize=(10, 10))
     for i in range(number):
