e07bcb13da55f4e2355851d5e18f82d170ebb8ab
Nelson-Gon <gonzabato@hotmail.com>
2020-07-01 20:50:53 +0800

    init file

---
+++ unet/__init__.py
@@ -0,0 +1,19 @@
+"""
+
+A Keras-Tensorflow Image Segmentation Pipeline
+
+.. author: Nelson Gonzabato
+
+"""
+
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+__author__="Nelson Gonzabato"
+__version__="0.1.0".encode("ascii", "ignore").decode('ascii')
+__all__=["model", "data", "augmentation"]
+
+
+
+
