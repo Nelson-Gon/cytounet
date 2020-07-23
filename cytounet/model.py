6ac27e78b8d96e5ea7030d0917db9166490b90dd
Nelson-Gon <gonzabato@hotmail.com>
2020-07-23 18:32:25 +0800

    Drop dropout

--- cytounet/model.py
+++ cytounet/model.py
@@ -41,9 +41,8 @@ def dice_coef_loss(y_true, y_pred):
 
 # bias variance tradeoff
 # https://blog.insightdatascience.com/bias-variance-tradeoff-explained-fa2bc28174c4
-def unet_simple(pretrained_weights=None, input_size=(256, 256, 1),optimiser="Adam",
-                learning_rate=3e-6, loss="binary_crossentropy",
-         metrics=["accuracy"],dropout_rate = 0.5):
+def unet_simple(pretrained_weights=None, input_size=(256, 256, 1), optimiser="Adam",
+                learning_rate=3e-6, loss="binary_crossentropy"):
     """
     :param pretrained_weights: If a pretrained model exists, provide it here for fine tuning
     :param input_size: size of the first layer(input_layer). Defaults to (256, 256, 1)
@@ -52,31 +51,28 @@ def unet_simple(pretrained_weights=None, input_size=(256, 256, 1),optimiser="Ada
     :param loss:  Loss function to use. Defaults to binary_crossentropy
     :param metrics: Metrics to use. Defaults to dice_coef
     :return: A simple(r) unet model.
-
     """
     inputs = Input(shape=input_size)
-    conv1 = Conv2D(64, 3, activation="relu", padding="same",kernel_initializer="he_normal")(inputs)
-    conv1 =BatchNormalization()(conv1)
-    conv1 = Conv2D(64, 3, activation="relu", padding="same",kernel_initializer="he_normal")(conv1)
-    conv1 =BatchNormalization()(conv1)
+    conv1 = Conv2D(64, 3, activation="relu", padding="same", kernel_initializer="he_normal")(inputs)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(64, 3, activation="relu", padding="same", kernel_initializer="he_normal")(conv1)
 
-    conv2 = Conv2D(128, 3, activation="relu", padding="same",kernel_initializer='he_normal')(conv1)
+    conv2 = Conv2D(128, 3, activation="relu", padding="same", kernel_initializer='he_normal')(conv1)
     conv2 = BatchNormalization()(conv2)
-    conv2 = Conv2D(128, 3, activation="relu", padding="same",kernel_initializer='he_normal')(conv2)
+    conv2 = Conv2D(128, 3, activation="relu", padding="same", kernel_initializer='he_normal')(conv2)
     conv2 = BatchNormalization()(conv2)
-    conv2 = Dropout(dropout_rate)(conv2)
 
-    up3 = Conv2D(64, 2, activation="relu", padding="same",kernel_initializer="he_normal")(conv2)
+    up3 = Conv2D(64, 2, activation="relu", padding="same", kernel_initializer="he_normal")(conv2)
     merge3 = concatenate([up3, conv1], axis=3)
     conv4 = Conv2D(1, 1, activation="relu")(merge3)
 
     model = Model(inputs=inputs, outputs=conv4)
 
     optimiser_list = {'Adam': Adam(lr=learning_rate),
-                      'SGD': SGD(learning_rate=learning_rate, momentum=0.99) }
+                      'SGD': SGD(learning_rate=learning_rate, momentum=0.99)}
 
     model.compile(optimizer=optimiser_list[optimiser],
-                  loss=loss, metrics = metrics)
+                  loss=loss, metrics=metrics)
 
     if pretrained_weights is not None:
         model.load_weights(pretrained_weights)
@@ -84,11 +80,9 @@ def unet_simple(pretrained_weights=None, input_size=(256, 256, 1),optimiser="Ada
     return model
 
 
-
-def unet(pretrained_weights=None, input_size=(256, 256, 1),optimiser="SGD", learning_rate=1e-4, loss=dice_coef_loss,
-         metrics=[dice_coef], dropout_rate = 0.2):
+def unet(pretrained_weights=None, input_size=(256, 256, 1), optimiser="SGD", learning_rate=1e-4, loss=dice_coef_loss,
+         metrics=[dice_coef]):
     """
-
     :param pretrained_weights: If a pretrained model exists, provide it here for fine tuning
     :param input_size: size of the first layer(input_layer). Defaults to (256, 256, 1)
     :param optimiser: Optimiser to use. One of SGD or Adam. Defaults to SGD.
@@ -96,10 +90,9 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1),optimiser="SGD", lear
     :param loss:  Loss function to use. Defaults to dice_coef_loss
     :param metrics: Metrics to use. Defaults to dice_coef
     :return: A unet model.
-
     """
     # momentum chosen based on the original paper
-    optimiser_list = {'Adam': Adam(lr=learning_rate), 'SGD': SGD(learning_rate=learning_rate,momentum=0.99)}
+    optimiser_list = {'Adam': Adam(lr=learning_rate), 'SGD': SGD(learning_rate=learning_rate, momentum=0.99)}
     inputs = Input(input_size)
     conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
     conv1 = BatchNormalization()(conv1)
@@ -120,18 +113,16 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1),optimiser="SGD", lear
     conv4 = BatchNormalization()(conv4)
     conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
     conv4 = BatchNormalization()(conv4)
-    drop4 = Dropout(dropout_rate)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
     conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
     conv5 = BatchNormalization()(conv5)
     conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
     conv5 = BatchNormalization()(conv5)
-    drop5 = Dropout(dropout_rate)(conv5)
 
     up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(drop5))
-    merge6 = concatenate([drop4, up6], axis=3)
+        UpSampling2D(size=(2, 2))(conv5))
+    merge6 = concatenate([conv4, up6], axis=3)
     conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
     conv6 = BatchNormalization()(conv6)
     conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
@@ -150,19 +141,19 @@ def unet(pretrained_weights=None, input_size=(256, 256, 1),optimiser="SGD", lear
     merge8 = concatenate([conv2, up8], axis=3)
     conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
     conv8 = BatchNormalization()(conv8)
-    conv8 = Conv2D(128, 3, activation='relu',padding='same', kernel_initializer='he_normal')(conv8)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
     conv8 = BatchNormalization()(conv8)
 
-    up9 = Conv2D(64, 2, activation='relu',padding='same', kernel_initializer='he_normal')(
+    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
         UpSampling2D(size=(2, 2))(conv8))
     merge9 = concatenate([conv1, up9], axis=3)
     conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
     conv9 = BatchNormalization()(conv9)
-    conv9 = Conv2D(64, 3,activation='relu',  padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
     conv9 = BatchNormalization()(conv9)
-    conv9 = Conv2D(2, 3,activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
     conv9 = BatchNormalization()(conv9)
-    conv10 = Conv2D(1, 1,activation='relu')(conv9)
+    conv10 = Conv2D(1, 1, activation='relu')(conv9)
 
     model = Model(inputs=inputs, outputs=conv10)
 
